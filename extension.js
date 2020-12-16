74063f50e87f161d17efa2445d6fe0e2cd0bfe2a
corey <ctrager@yahoo.com>
2020-12-16 09:24:17 -0600

    playing

--- extension.js
+++ extension.js
@@ -1,3 +1,10 @@
+// Links related to user input
+// https://stackoverflow.com/questions/36705126/vscode-extension-how-to-display-a-form
+// https://github.com/microsoft/vscode-extension-samples/blob/master/quickinput-sample/src/multiStepInput.ts
+// https://code.visualstudio.com/api/extension-guides/webview
+
+
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -25,11 +32,14 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		console.log("corey2", Date.now())
 		vscode.window.showInformationMessage('Corey says Hello World from vscee!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	// Corey says...pretend something happens
+	// setTimeout(corey_something_happened, 500)
+	setTimeout(corey_create_webview_panel, 500);
 }
 exports.activate = activate;
 
@@ -41,107 +51,88 @@ module.exports = {
 	deactivate
 }
 
-setInterval(corey_callback, 2000)
+// showInformationMessage
+async function corey_something_happened() {
 
-counter = 0
-function corey_callback() {
-	counter++
-	console.log("corey callback", Date.now(), counter);
-	// if (counter == 1) {
-	// 	create_my_webview_panel()
-	// }
+	console.log("corey_something_happened");
 
-	if (counter == 1) {
-		my_show_input_box()
+	await vscode.window.showInformationMessage("Chose one or two",
+		"show input box",
+		"don't show").then(choice => {
+
+			console.log("after info")
+			vscode.window.showInformationMessage(`you chose ${choice}`)
+
+			if (choice == "show input box") {
+				corey_show_input_box()
+			}
+		})
+}
+
+// showInputBox
+async function corey_show_input_box() {
+
+	console.log("corey_show_input_box")
+
+	await vscode.window.showInputBox({
+		value: "red",
+		placeHolder: "Enter your favorite color - try 'cat'",
+		validateInput: text => {
+			return text === "cat" ? "cat is not a color!" : null
+		}
+	}).then(text => {
+
+		console.log("after input")
+		vscode.window.showInformationMessage(text)
+		corey_show_quick_pick()
+	});
+
+}
+
+
+// showQuickPick
+async function corey_show_quick_pick() {
+
+	console.log("corey_show_quick_pick")
+
+	var items = ["corey", "misayo", "abi", "isaac"]
+	var options = {canPickMany: true, placeHolder: "who do you love?"}
+	await vscode.window.showQuickPick(items, options).then(selection => {
+
+		console.log("after quick pick")
+		vscode.window.showInformationMessage(JSON.stringify(selection))
+		corey_create_webview_panel()
+	});
+
+}
+
+// createWebViewPanel
+var panel = null
+
+function corey_create_webview_panel() {
+	console.log("corey_create_webview_panel")
+
+	var show_options = {
+		viewColumn: vscode.ViewColumn.One,
+		preserveFocus: false
 	}
 
-}
+	var options = {
+		enableScripts: true,
+		localResourceRoots: [vscode.Uri.file(path.join(my_context.extensionPath, 'webview'))]
+	}
 
-async function my_show_input_box() {
-	const result = await vscode.window.showInputBox({
-		value: 'abcdef',
-		valueSelection: [2, 4],
-		placeHolder: 'For example: fedcba. But not: 123',
-		validateInput: text => {
-			//vscode.window.showInformationMessage(`Validating: ${text}`);
-			return text === '123' ? 'Not 123!' : null;
-		}
-	});
-	vscode.window.showInformationMessage(`Got: ${result}`, "my action one", "my action two");
-
-
-}
-panel = null
-function create_my_webview_panel() {
-	console.log("webview 1")
 	panel = vscode.window.createWebviewPanel(
 		"corey view type",
 		'corey title',
-		{
-			viewColumn: vscode.ViewColumn.One,
-			preserveFocus: true
-		},
-		{
-			enableScripts: true,
-			localResourceRoots: [vscode.Uri.file(path.join(my_context.extensionPath, 'webview'))]
-		}
-		// column || vscode.ViewColumn.One,
-		// {
-		// 	// Enable javascript in the webview
-		// 	//enableScripts: true,
+		show_options,
+		options);
 
-		// 	// And restrict the webview to only loading content from our extension's `media` directory.
-		//localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
-		// }
-	);
+	console.log("after create webview panel")
 
-	console.log("webview 2")
-	console.log("path", my_context.extensionPath)
 	vscode.workspace.openTextDocument(vscode.Uri.file(my_context.extensionPath + "/webview/my_webview.html"))
 		.then(doc => {
-			console.log("opening doc worked!!!!!!!!!!!!!")
+			console.log("after open text document")
 			panel.webview.html = doc.getText()
 		})
-
-	//	openTextDocument(uri: Uri): Thenable<TextDocument>
-	//panel.webview.html = getWebviewContent();
-
-}
-
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-	  <script>
-	  function on_load() {
-		  console.log("corey is logging from on load")
-	  }
-	  function foo() {
-		  document.getElementById("hidden_div").style.display = "block"
-		  console.log("corey click")
-	  }
-	</script>
-  </head>
-  <body onload="on_load()">
-	  <div style="border: 3px color red; background-color:yellow;">Corey is here!</div>
-	  <form>
-			number? <input type=number/>
-
-			<br>
-			color?
-		  <select>
-		  <option>red</option>
-		  <option>green</option>
-		  <option>blue</option>
-		  </select>
-	  </form>
-	  <br>
-	  <button onclick="foo()">click me</button>
-	  <div id="hidden_div" style="display:none">You can see me now</div>
-
-  </body>
-  </html>`;
 }
